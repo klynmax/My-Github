2169dceb5dec454dfa1f3829692e39c9d50bb3fc
klynsman <klynsman.lemos@gmail.com>
2022-02-13 17:19:21 -0300

    Ajuste no código

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -78,6 +78,7 @@ export default function PermanentDrawerLeft(props) {
           direction="row"
           justifyContent="center"
           alignItems="center"
+          item
         >
           <img 
             src={Logo} 
